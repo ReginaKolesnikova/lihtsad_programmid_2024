b950cb63ad44b003d0d48733509cf37241411cb4
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2024-11-27 16:30:09 +0200

    lihtsad_programmid.py

--- lihtsad_programmid_2024/module1.py
+++ lihtsad_programmid_2024/module1.py
@@ -1,1 +1,17 @@
 
+from datetime import *
+from math import *
+
+# Ulesanne 1
+tana=date.today()
+tanaf=date.today().strftime("%B %d, %Y")
+
+print(f"Tere! Täna on {tanaf}")
+d=tana.day
+m=tana.month
+y=tana.year
+print(d)
+print(m)
+print(y)
+
+
